93d0490b916378c4beff600de4ea01cb6ff1cb6f
Jeremiah Lee <jeremiah@sourceninja.com>
2012-06-21 10:53:07 -0700

    Initial check-in of a mostly-working script to scrape maven central search for POM information.

---
+++ search_scraper.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+unless(-e 'json' and -d _) {
+	mkdir 'json';
+}
+
+my $NUMBER_FOUND = 41_447;
+
+my $PER_PAGE = 100;
+
+my $start = 0;
+while( $start < $NUMBER_FOUND ) {
+	my $url = 'http://search.maven.org/solrsearch/select?q=.pom&rows=' . $PER_PAGE . '&wt=json&start=' . $start;
+	my $end = $start + $PER_PAGE - 1;
+
+	my $wget = "wget -O 'json/$start-$end.json' $url";
+
+	print $wget, "\n";
+	system $wget;
+
+	$start += $PER_PAGE;
+	sleep 5;
+}
